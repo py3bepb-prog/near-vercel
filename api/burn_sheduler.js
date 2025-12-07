b8358e1b68b80284d1b9db3f701887e941ab5dee
py3bepb-prog <py3bepb@gmail.com>
2025-12-06 23:10:18 -0300

    Добавляем Burn

--- api/burn_sheduler.js
+++
@@ -1,7 +0,0 @@
-// api/burn_scheduler.js
-module.exports = async (req, res) => {
-    // Просто отправляем текстовый ответ, чтобы проверить, что маршрут доступен
-    res.setHeader('Content-Type', 'text/plain');
-    res.statusCode = 200;
-    res.end('Route is OK.');
-};
